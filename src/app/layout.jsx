61b30c6574f08b9b9caceaa138093de2e10fc4f2
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-12 09:54:18 -0300

    Styles to navbar and footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-import Navbar from '@/components/Navbar';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar/Navbar';
+import Footer from '@/components/Footer/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 

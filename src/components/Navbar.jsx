9d36b67f2c618defda32264017216d6a53fe6f56
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-12 09:50:40 -0300

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+
+const links = [
+  {
+    id: 1,
+    title: 'Home',
+    url: '/',
+  },
+  {
+    id: 2,
+    title: 'Portfolio',
+    url: '/portfolio',
+  },
+  {
+    id: 3,
+    title: 'Blog',
+    url: '/blog',
+  },
+  {
+    id: 4,
+    title: 'About',
+    url: '/about',
+  },
+  {
+    id: 5,
+    title: 'Contact',
+    url: '/contact',
+  },
+  {
+    id: 6,
+    title: 'Dashboard',
+    url: '/dashboard',
+  },
+];
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <Link href={link.url}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;

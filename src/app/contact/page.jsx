56983a1f96ba03687c62cc5b513f84e771fec892
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-27 07:46:07 -0300

    Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,34 @@
+import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 function ContactPage() {
-  return <div className={styles.container}>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's get in touch!</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/contact.png'
+            alt='Contact image'
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='Name' className={styles.input} />
+          <input type='text' placeholder='E-mail' className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder='Message'
+          ></textarea>
+          <Link href='#' className={styles.btn}>
+            Submit
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ContactPage;

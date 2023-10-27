f8212336ca44bd525701eab754ad1c80ddafcd7c
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-27 09:00:26 -0300

    Portfolio page created

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 function Portfolio() {
-  return <div className={styles.container}>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery!</h1>
+      <div className={styles.items}>
+        <Link href='/portfolio/timeline' className={styles.item}>
+          <span className={styles.itemTitle}>Timeline</span>
+        </Link>
+        <Link href='/portfolio/websites' className={styles.item}>
+          <span className={styles.itemTitle}>Websites</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;

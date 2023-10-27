11af73e1d41f462395146fc8c6766ac7969b22dd
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-27 09:00:46 -0300

    structure of the category param

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,5 +1,23 @@
-function PortfolioCategory() {
-  return <div>PortfolioCategory</div>;
+import Button from '@/components/Button/Button';
+import styles from './page.module.css';
+import Image from 'next/image';
+
+function PortfolioCategory({ params }) {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.paragraph}>Test</p>
+          <Button link='#' text='Test' />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src='' alt='' fill={true} className={styles.img} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PortfolioCategory;

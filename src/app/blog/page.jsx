d095fe061874e28935cb390c22899fbe869d4337
Tomás Arrativel <arrativeltomas@gmail.com>
2023-10-30 11:22:28 -0300

    getData function to see fetched data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,56 +2,37 @@ import Link from 'next/link';
 import styles from './page.module.css';
 import Image from 'next/image';
 
-function BlogPage() {
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  return res.json();
+}
+
+async function BlogPage() {
+  const data = await getData();
   return (
     <div className={styles.container}>
-      <Link href='/blog/testId' className={styles.postContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/2119706/pexels-photo-2119706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            alt='street art in snowy wall'
-            height={250}
-            width={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.description}>Description</p>
-        </div>
-      </Link>
-
-      <Link href='/blog/testId' className={styles.postContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/2119706/pexels-photo-2119706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            alt='street art in snowy wall'
-            height={250}
-            width={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.description}>Description</p>
-        </div>
-      </Link>
-
-      <Link href='/blog/testId' className={styles.postContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/2119706/pexels-photo-2119706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            alt='street art in snowy wall'
-            height={250}
-            width={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.description}>Description</p>
-        </div>
-      </Link>
+      {data.map((item) => {
+        <Link
+          href='/blog/testId'
+          className={styles.postContainer}
+          key={item.id}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src='https://images.pexels.com/photos/2119706/pexels-photo-2119706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt='street art in snowy wall'
+              height={250}
+              width={400}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.description}>description</p>
+          </div>
+        </Link>;
+      })}
     </div>
   );
 }

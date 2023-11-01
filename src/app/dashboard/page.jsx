77ed211c824ccfb8f733a66890af0d32f18e70dd
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-01 08:53:50 -0300

    Fetch data with useState and useEffect

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,34 @@
+'use client';
+import { useEffect, useState } from 'react';
 import styles from './page.module.css';
+import useSWR from 'swr';
 
 function DashboardPage() {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        cache: 'no-store',
+      });
+
+      if (!res.ok) {
+        setErr(true);
+      }
+
+      const data = await res.json();
+
+      setData(data);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  console.log(data);
+
   return <div className={styles.container}>DashboardPage</div>;
 }
 

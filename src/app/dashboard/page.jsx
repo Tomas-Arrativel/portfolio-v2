d15d5417a43553642ab0685662b081a1875a7652
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-01 08:54:33 -0300

    Fetching data using SWR (easy way)

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,28 +4,36 @@ import styles from './page.module.css';
 import useSWR from 'swr';
 
 function DashboardPage() {
-  const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  // Using SWR
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        cache: 'no-store',
-      });
+  const { data, error, isLoading } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts',
+    fetcher,
+  );
 
-      if (!res.ok) {
-        setErr(true);
-      }
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-      const data = await res.json();
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  //       cache: 'no-store',
+  //     });
 
-      setData(data);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
 
   console.log(data);
 

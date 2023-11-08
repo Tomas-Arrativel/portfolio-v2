a8cb49bcdfa17dc776d6f3342a5e468e2a781256
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-08 11:30:50 -0300

    Function GET from api

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,19 @@
+import Post from '@/models/Post';
+import connect from '@/utils/db';
+
+import { NextResponse } from 'next/server';
+
+export const GET = async (request) => {
+  // fetch data from mongodb
+  try {
+    await connect();
+    const posts = await Post.find();
+
+    return new NextResponse(posts, { status: 200 });
+  } catch (err) {
+    return new NextResponse('Error fetching data from database', {
+      status: 500,
+    });
+  }
+  // if ok return response
+};
